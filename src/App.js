c6bcdacdbf5ca897ea3b3505459de293dcdc41bb
ZIPcde <votmoyyashik100@gmail.com>
2024-06-08 23:55:04 +0300

    deploy2Router checked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import "./App.css";
 import Index from './components/index/index';
 import Catalog from './components/Catalog/Catalog';
