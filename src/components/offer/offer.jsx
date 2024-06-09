b305c844f61636f3ecda39e4141e73016fea48f5
ZIPcde <votmoyyashik100@gmail.com>
2024-06-10 01:00:40 +0300

    suffering with offer block

--- src/components/offer/offer.jsx
+++ src/components/offer/offer.jsx
@@ -9,6 +9,8 @@ const Offer = () => {
                 <div className="picks3">
                     <div className="div1of3">
                         <div className="internalpict picture1">
+                        <div className="offer_bcg-black"></div>
+                            <img className='offer_picture' src={require('./img/Layer 43.png')} alt="for women" />
                             <p className="offerout_paragr"><span className="offerout_sp4"> 30% OFF </span><br /><span className="offerout_sp3">FOR WOMEN</span>
                             </p>
 
@@ -16,6 +18,8 @@ const Offer = () => {
                     </div>
                     <div className="div1of3">
                         <div className="internalpict picture2">
+                        <div className="offer_bcg-black"></div>
+                            <img className='offer_picture' src={require('./img/Layer 30.png')} alt="for men" />
                             <p className="offerout_paragr"><span className="offerout_sp4">HOT DEAL
                             </span><br /><span className="offerout_sp3"><span> </span> FOR MEN</span></p>
 
@@ -23,6 +27,8 @@ const Offer = () => {
                     </div>
                     <div className="div1of3">
                         <div className="internalpict picture3">
+                        <div className="offer_bcg-black"></div>
+                            <img className='offer_picture' src={require('./img/Layer 44.png')} alt="for kids" />
                             <p className="offerout_paragr"><span className="offerout_sp4">NEW ARRIVALS
                             </span><br /><span className="offerout_sp3">FOR KIDS</span></p>
 
@@ -30,6 +36,8 @@ const Offer = () => {
                     </div>
                 <div className="wide">
                     <div className="internalpict2 picture4">
+                        <div className="offer_bcg-black"></div>
+                        <img className='offer_picture-wide' src={require('./img/Layer 45.png')} alt="ACCESORIES" />
                         <p className="offerout_paragr"><span className="offerout_sp4">LUXIROUS & TRENDY
                         </span><br /><span className="offerout_sp3">ACCESORIES</span></p>
                     </div>
